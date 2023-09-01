4835e8568c8a657769a08fb9aaf14cd6c2b507ae
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-01 13:56:18 +0530

    Add files via upload

---
+++ file.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <string.h>
+#include "trie.c"
+#define NUM_CHAR 26
+
+
+int main(){
+
+
+    printf("Enter Word List :");
+
+    int productsSize;
+    scanf("%d", &productsSize);
+    
+    char **products = (char **)malloc(productsSize * sizeof(char *));
+
+    for (int i = 0; i < productsSize; i++) {
+        char buffer[100];
+        scanf("%s", buffer);
+        products[i] = (char *)malloc(strlen(buffer) + 1);
+        strcpy(products[i], buffer);
+    }
+    
+    char searchWord[100];
+
+    scanf("%s", searchWord);
+
+    Trie *root = createTrie();
+    for(int i=0;i<productsSize;i++) {
+        insert(root, products[i]);
+    }
+
+    int search_len = strlen(searchWord);
+    char str[20];
+    int k = 0;
+    for(int i=0; i<20; i++) str[i] = '\0';
+
+    for(int i=0;i<search_len;i++) {
+        str[k++] = searchWord[i];
+        printf("%s- ", str);
+        suggestion(root, searchWord, 0, i);
+        printf("\n");
+    }
+}
---
+++ trie.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <string.h>
+#define NUM_CHAR 26
+
+
+typedef struct Trie{
+
+    struct Trie *links[NUM_CHAR];
+    bool isEnd;
+    char *text;
+
+} Trie;
+
+Trie* createTrie() {
+    
+    Trie *node = (Trie *)malloc(sizeof(Trie));
+    for(int i=0;i<NUM_CHAR;i++) {
+        node->links[i] = NULL;
+    }
+    node->isEnd = false;
+    node->text = NULL;
+
+    return node;
+}
+
+void insert(Trie *node, char *word) {
+    
+    int length = strlen(word);
+    Trie *temp = node;
+
+    for(int i=0;i<length;i++) {
+
+        int letter = word[i]-'a';
+
+        if(temp->links[letter] == NULL) {
+            temp->links[letter] = createTrie();
+        }
+        temp = temp->links[letter];
+    }
+
+    if(temp->isEnd) return;
+    
+    temp->isEnd = true;
+    temp->text = word;
+}
+
+void suggestion(Trie *node, char *searchword, int k, int n) {
+    
+    if(node->isEnd){
+        printf("%s ", node->text);
+    }
+
+    for(int i=0;i<NUM_CHAR;i++){
+
+        if(node->links[i] != NULL){
+
+            int prefixChar = (searchword[k]-'a');
+            if(k > n || prefixChar == i) {
+                suggestion(node->links[i], searchword, k+1, n);
+            }
+        }
+    }
+}
