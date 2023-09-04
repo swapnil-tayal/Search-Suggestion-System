4a424fdc012611da8a65652796749e0cf643dc39
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-04 10:11:39 +0530

    Update file.c

--- file.c
+++ file.c
@@ -9,14 +9,15 @@
 
 int main(){
 
+    printf("Enter Word List : ");
 
-    printf("Enter Word List :");
-
+    // product list size
     int productsSize;
     scanf("%d", &productsSize);
     
     char **products = (char **)malloc(productsSize * sizeof(char *));
 
+    // input words
     for (int i = 0; i < productsSize; i++) {
         char buffer[100];
         scanf("%s", buffer);
@@ -24,10 +25,13 @@ int main(){
         strcpy(products[i], buffer);
     }
     
+    // search word
     char searchWord[100];
-    printf("Enter the Seach Word ");
+    printf("Enter the Seach Word : ");
     scanf("%s", searchWord);
+    printf("\n");
 
+    // intializaing trie 
     Trie *root = createTrie();
     for(int i=0;i<productsSize;i++) {
         insert(root, products[i]);
@@ -40,8 +44,24 @@ int main(){
 
     for(int i=0;i<search_len;i++) {
         str[k++] = searchWord[i];
-        printf("Words with prefix %s\n", str);
-        suggestion(root, searchWord, 0, i);
+
+        // GUI 
+        for(int j=0; j<40; j++) printf("-");
         printf("\n");
+        printf("|");
+        int spaces = (38 - (k+7) + 1)/2;
+        for(int j=0; j<spaces; j++) printf(" ");
+        printf("Prefix %s", str);
+        for(int j=0; j< 38-(spaces+7+k); j++) printf(" ");
+        printf("|\n");
+        for(int j=0; j<40; j++) printf("-");
+        printf("\n");
+
+        // suggestion call 
+        suggestion(root, searchWord, 0, i);
+        
+        // GUI
+        for(int j=0; j<40; j++) printf("-");
+        printf("\n\n");
     }
 }
